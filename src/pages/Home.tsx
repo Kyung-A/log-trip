b10d39050723bd85e83a831a886dd3cad3de83e1
NamEunGyeong <nek1717@naver.com>
2025-06-27 16:45:31 +0900

    도시 선택 ui 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,14 +16,30 @@ import BottomSheet, {
 } from "@gorhom/bottom-sheet";
 import EvilIcons from "react-native-vector-icons/EvilIcons";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import Swiper from "react-native-web-swiper";
+import { COUNTRIES } from "@/constants";
 
 const SNAP_POINTS = ["100%"];
 
+interface ICountry {
+  name: string;
+  code: string;
+  country: string;
+  countryName: string;
+}
+
 export default function HomeScreen() {
-  const bottomSheetRef = useRef<BottomSheet>(null);
   const { showActionSheetWithOptions } = useActionSheet();
+
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
   const [imgs, setImgs] = useState<string[] | null>(null);
+  const [selectedCountries, setSelectedCountries] = useState<ICountry[]>([]);
+  const [searchCountries, setSearchCountries] = useState<ICountry[]>(COUNTRIES);
+  const [resultSelectedCountries, setResultSelectedCountries] = useState<
+    ICountry[]
+  >([]);
 
   const handleDeleted = useCallback(
     (uri: string) => {
@@ -81,9 +97,49 @@ export default function HomeScreen() {
     bottomSheetRef.current?.expand();
   }, []);
 
-  const handleSheetChanges = useCallback((index: number) => {
-    console.log("handleSheetChanges", index);
+  const handleSheetChanges = useCallback(
+    (index: number) => {
+      if (index == -1) {
+        setSelectedCountries(resultSelectedCountries);
+      }
+    },
+    [resultSelectedCountries]
+  );
+
+  const handleSelectedCountry = useCallback((selectData: ICountry) => {
+    setSelectedCountries((prev) => {
+      const isSelected = prev.some(
+        (data) =>
+          data.code === selectData.code && data.country === selectData.country
+      );
+
+      if (isSelected) {
+        return prev.filter(
+          (p) =>
+            !(p.code === selectData.code && p.country === selectData.country)
+        );
+      } else {
+        return [...prev, selectData];
+      }
+    });
   }, []);
+
+  const handleSearchCountry = useCallback((value) => {
+    const newList = COUNTRIES.filter(
+      (v) => v.name.includes(value) || v.countryName.includes(value)
+    );
+    setSearchCountries(newList);
+  }, []);
+
+  const handleSelectedFinish = useCallback(() => {
+    bottomSheetRef.current.close();
+    setResultSelectedCountries(selectedCountries);
+  }, [selectedCountries]);
+
+  const handleCloseBottomSheet = useCallback(() => {
+    bottomSheetRef.current.close();
+    setSelectedCountries(resultSelectedCountries);
+  }, [resultSelectedCountries]);
 
   return (
     <>
@@ -121,14 +177,19 @@ export default function HomeScreen() {
           )}
         </View>
 
-        <View>
-          <Pressable
-            onPress={handleOpenPress}
-            className="w-full p-4 border-b border-gray-300"
-          >
-            <Text className="text-xl">도시 선택</Text>
-          </Pressable>
-        </View>
+        <Pressable
+          onPress={handleOpenPress}
+          className="flex flex-row flex-wrap items-start justify-between w-full p-4 border-b border-gray-300"
+        >
+          <Text className="mr-4 text-xl">도시 선택</Text>
+          <View className="flex flex-row flex-wrap flex-1 gap-2">
+            {resultSelectedCountries.map((v) => (
+              <Text className="p-2 rounded bg-[#ebebeb] font-semibold">
+                {v.name}
+              </Text>
+            ))}
+          </View>
+        </Pressable>
       </ScrollView>
 
       <BottomSheet
@@ -145,21 +206,66 @@ export default function HomeScreen() {
           />
         )}
       >
-        <BottomSheetScrollView className="flex-1 px-6 bg-white">
-          <View className="flex flex-row items-center justify-between w-full">
-            <Pressable>
-              <Ionicons name="close-outline" size={32} color="#000" />
-            </Pressable>
-            <Text className="text-xl font-semibold">도시 선택</Text>
-            <Button title="완료" />
+        <BottomSheetScrollView
+          stickyHeaderIndices={[0]}
+          className="flex-1 bg-white"
+        >
+          <View className="sticky top-0 h-auto px-6 pb-4 bg-white border-b border-[#ebebeb]">
+            <View className="flex flex-row items-center justify-between w-full">
+              <Pressable onPress={handleCloseBottomSheet}>
+                <Ionicons name="close-outline" size={32} color="#000" />
+              </Pressable>
+              <Text className="text-xl font-semibold">도시 선택</Text>
+              <Button onPress={handleSelectedFinish} title="완료" />
+            </View>
+            <View className="flex flex-row items-center px-3 py-4 mt-6 rounded-lg bg-[#ebebeb]">
+              <Ionicons name="search-outline" size={24} />
+              <TextInput
+                className="ml-3 text-lg"
+                textContentType="addressCity"
+                placeholder="도시 또는 나라 검색"
+                onChangeText={(e) => handleSearchCountry(e)}
+              />
+            </View>
+            <View className="flex flex-row flex-wrap gap-2 mt-4">
+              {selectedCountries.map((v) => (
+                <Text
+                  key={`${v.code}-${v.country}`}
+                  className="p-2 rounded bg-[#ebebeb] font-semibold"
+                >
+                  {v.name}
+                </Text>
+              ))}
+            </View>
           </View>
-          <View className="flex flex-row items-center px-3 py-4 mt-6 rounded-lg bg-zinc-100">
-            <Ionicons name="search-outline" size={24} />
-            <TextInput
-              className="ml-3 text-lg"
-              textContentType="addressCity"
-              placeholder="도시 또는 나라 검색"
-            />
+          <View className="flex flex-col min-h-screen px-6 py-2 gap-y-6">
+            {searchCountries.map((v, idx) => {
+              const selected = selectedCountries.some(
+                (item) => item.code === v.code && item.country === v.country
+              );
+
+              return (
+                <Pressable
+                  key={`${v.code}-${v.country}-${v.name}-${idx}`}
+                  onPress={() => handleSelectedCountry(v)}
+                  className="flex flex-row items-center gap-x-2"
+                >
+                  <MaterialCommunityIcons
+                    name={
+                      selected ? "checkbox-marked" : "checkbox-blank-outline"
+                    }
+                    size={28}
+                    color={selected ? "#000" : "#ccc"}
+                  />
+                  <View>
+                    <Text className="text-xl">{v.name}</Text>
+                    <Text className="mt-0.5 text-base text-gray-600">
+                      {v.countryName}
+                    </Text>
+                  </View>
+                </Pressable>
+              );
+            })}
           </View>
         </BottomSheetScrollView>
       </BottomSheet>
