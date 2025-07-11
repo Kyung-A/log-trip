13efdbcec784a0c0689aacbf7de8d8194e5600bd
NamEunGyeong <nek1717@naver.com>
2025-07-11 13:10:37 +0900

    맵 박스 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import MapboxGL from "@rnmapbox/maps";
+
+MapboxGL.setAccessToken(process.env.MAPBOX_KEY);
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={{ flex: 1 }}>
+      <MapboxGL.MapView
+        styleURL={process.env.MAPBOX_STYLE_URL}
+        style={{ flex: 1 }}
+      >
+        <MapboxGL.Camera centerCoordinate={[126.978, 37.5665]} zoomLevel={5} />
+      </MapboxGL.MapView>
     </View>
   );
 }
