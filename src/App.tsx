c634393ba02ca1acd28151b1da10531bba71ab42
Kyung-A <nek1717@naver.com>
2025-07-19 23:27:14 +0900

    router, 네비게이션 수정

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import DiaryCreateScreen from "./pages/DiaryCreate";
+import ProfileUpdateScreen from "./pages/ProfileUpdate";
+import FontAwesome6 from "react-native-vector-icons/FontAwesome6";
+import { Text, TouchableOpacity } from "react-native";
 
 import "./global.css";
 
@@ -23,7 +26,42 @@ export default function App() {
                 component={TabBar}
                 options={{ headerShown: false }}
               />
-              <Stack.Screen name="DiaryCreate" component={DiaryCreateScreen} />
+              <Stack.Screen
+                name="DiaryCreate"
+                component={DiaryCreateScreen}
+                options={({ navigation }) => ({
+                  headerTitle: () => (
+                    <Text className="text-lg font-semibold">일기 작성</Text>
+                  ),
+                  headerLeft: () => (
+                    <TouchableOpacity onPress={() => navigation.goBack()}>
+                      <FontAwesome6
+                        name="arrow-left"
+                        size={20}
+                        color="#646464"
+                      />
+                    </TouchableOpacity>
+                  ),
+                })}
+              />
+              <Stack.Screen
+                name="ProfileUpdate"
+                component={ProfileUpdateScreen}
+                options={({ navigation }) => ({
+                  headerTitle: () => (
+                    <Text className="text-lg font-semibold">프로필 수정</Text>
+                  ),
+                  headerLeft: () => (
+                    <TouchableOpacity onPress={() => navigation.goBack()}>
+                      <FontAwesome6
+                        name="arrow-left"
+                        size={20}
+                        color="#646464"
+                      />
+                    </TouchableOpacity>
+                  ),
+                })}
+              />
             </Stack.Navigator>
           </NavigationContainer>
         </BottomSheetModalProvider>
--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -1,14 +1,94 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "@/pages/Home";
 import DiaryScreen from "@/pages/Diary";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import { View } from "react-native";
+import DummyScreen from "@/pages/Dummy";
+import MyPageScreen from "@/pages/MyPage";
+import CompanionScreen from "@/pages/Companion";
 
 const Tab = createBottomTabNavigator();
 
 export default function TabBar() {
   return (
-    <Tab.Navigator id={undefined}>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Diary" component={DiaryScreen} />
+    <Tab.Navigator
+      id={undefined}
+      screenOptions={({ route }) => ({
+        tabBarStyle: {
+          height: 80,
+          overflow: "visible",
+        },
+        tabBarIcon: ({ focused }) => {
+          if (route.name === "세계지도") {
+            return (
+              <Ionicons
+                name="map"
+                size={20}
+                color={focused ? "#a38f86" : "#A9A9A9"}
+              />
+            );
+          } else if (route.name === "동행") {
+            return (
+              <Ionicons
+                name="people"
+                size={20}
+                color={focused ? "#a38f86" : "#A9A9A9"}
+              />
+            );
+          } else if (route.name === "내여행") {
+            return (
+              <FontAwesome5
+                name="suitcase"
+                size={20}
+                color={focused ? "#a38f86" : "#A9A9A9"}
+              />
+            );
+          } else if (route.name === "마이페이지") {
+            return (
+              <Ionicons
+                name="person"
+                size={20}
+                color={focused ? "#a38f86" : "#A9A9A9"}
+              />
+            );
+          }
+        },
+        tabBarActiveTintColor: "#a38f86",
+        tabBarInactiveTintColor: "#A9A9A9",
+      })}
+    >
+      <Tab.Screen name="세계지도" component={HomeScreen} />
+      <Tab.Screen name="동행" component={CompanionScreen} />
+      <Tab.Screen
+        name="글쓰기"
+        component={DummyScreen}
+        options={{
+          tabBarLabel: "",
+          tabBarIcon: () => (
+            <View
+              style={{
+                width: 60,
+                position: "absolute",
+                top: -20,
+                justifyContent: "center",
+                alignItems: "center",
+                zIndex: 10,
+              }}
+            >
+              <Ionicons name="add-circle" size={60} color="#d5b2a8" />
+            </View>
+          ),
+        }}
+        listeners={({ navigation }) => ({
+          tabPress: (e) => {
+            (e as unknown as { preventDefault: () => void }).preventDefault();
+            navigation.navigate("DiaryCreate");
+          },
+        })}
+      />
+      <Tab.Screen name="내여행" component={DiaryScreen} />
+      <Tab.Screen name="마이페이지" component={MyPageScreen} />
     </Tab.Navigator>
   );
 }
