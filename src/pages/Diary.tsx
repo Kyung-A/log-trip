fc182e7f99872b9f619b3141a07d55863a1bfbaa
Kyung-A <nek1717@naver.com>
2025-08-29 13:26:56 +0900

    다이어리 삭제 액션 시트로 변경

--- src/pages/Diary.tsx
+++ src/pages/Diary.tsx
@@ -1,42 +1,41 @@
-import { Pressable, View, Text, Image, FlatList } from "react-native";
-import { DrawingiContentPost, TextContentPost } from "@/features/diary/ui";
-import Feather from "react-native-vector-icons/Feather";
-import { useCallback, useRef, useState } from "react";
-import BottomSheet, {
-  BottomSheetBackdrop,
-  BottomSheetView,
-} from "@gorhom/bottom-sheet";
-import Swiper from "react-native-web-swiper";
+import { Pressable, View, Text, Image, FlatList } from 'react-native';
+import { DrawingiContentPost, TextContentPost } from '@/features/diary/ui';
+import Feather from 'react-native-vector-icons/Feather';
+import { useCallback } from 'react';
+import Swiper from 'react-native-web-swiper';
 import {
   IDiary,
   useFetchDiaries,
   useMutationDeleteDiary,
-} from "@/entities/diary";
-import { useFetchUserId } from "@/entities/auth";
-
-const SNAP_POINTS = ["15%"];
+} from '@/entities/diary';
+import { useFetchUserId } from '@/entities/auth';
+import { useActionSheet } from '@expo/react-native-action-sheet';
 
 export default function DiaryScreen({ navigation }) {
-  const bottomSheetRef = useRef<BottomSheet>(null);
-
-  const [selectedPostId, setSelectedPostId] = useState<IDiary>();
+  const { showActionSheetWithOptions } = useActionSheet();
 
   const { data: userId } = useFetchUserId();
   const { data } = useFetchDiaries(userId);
   const { mutateAsync } = useMutationDeleteDiary();
 
-  const handleSheetChanges = useCallback((index: number) => {
-    if (index === -1) {
-      setSelectedPostId(null);
-    }
+  const handleDeleteDiary = useCallback(async (item: IDiary) => {
+    await mutateAsync(item);
   }, []);
 
-  const handleDeleteDiary = useCallback(async () => {
-    const { status } = await mutateAsync(selectedPostId);
-    if (status === 204) {
-      bottomSheetRef.current?.close();
-    }
-  }, [selectedPostId]);
+  const handleOpenActionSheet = useCallback((item: IDiary) => {
+    const options = ['삭제', '취소'];
+
+    showActionSheetWithOptions(
+      {
+        options,
+        cancelButtonIndex: 1,
+      },
+      idx => {
+        if (idx === 0) handleDeleteDiary(item);
+        else if (idx === 1) return;
+      },
+    );
+  }, []);
 
   return (
     <>
@@ -48,7 +47,7 @@ export default function DiaryScreen({ navigation }) {
             <View key={item.id} className="w-full h-auto mb-2 bg-white">
               <View className="flex-row items-center justify-between p-4">
                 <Pressable
-                  onPress={() => navigation.navigate("마이페이지")}
+                  onPress={() => navigation.navigate('마이페이지')}
                   className="flex-row items-center gap-x-2"
                 >
                   <View className="overflow-hidden rounded-full w-14 h-14">
@@ -59,12 +58,7 @@ export default function DiaryScreen({ navigation }) {
                   </View>
                   <Text>{item.user_info.name}</Text>
                 </Pressable>
-                <Pressable
-                  onPress={() => {
-                    bottomSheetRef.current?.expand();
-                    setSelectedPostId(item);
-                  }}
-                >
+                <Pressable onPress={() => handleOpenActionSheet(item)}>
                   <Feather name="more-vertical" size={20} />
                 </Pressable>
               </View>
@@ -75,18 +69,18 @@ export default function DiaryScreen({ navigation }) {
                     key="my"
                     loop
                     containerStyle={{
-                      width: "100%",
+                      width: '100%',
                       height: 400,
                     }}
                     controlsProps={{
                       prevPos: false,
                       nextPos: false,
                       dotsTouchable: true,
-                      dotsPos: "bottom",
-                      dotActiveStyle: { backgroundColor: "#d5b2a7" },
+                      dotsPos: 'bottom',
+                      dotActiveStyle: { backgroundColor: '#d5b2a7' },
                     }}
                   >
-                    {item.diary_images.map((img) => (
+                    {item.diary_images.map(img => (
                       <Image
                         key={img.id}
                         source={{ uri: img.url }}
@@ -108,39 +102,12 @@ export default function DiaryScreen({ navigation }) {
       ) : (
         <View className="items-center justify-center flex-1 gap-6">
           <Image
-            source={require("../../assets/images/logo.png")}
+            source={require('../../assets/images/logo.png')}
             className="object-cover w-32 h-32"
           />
           <Text className="text-lg">나만의 여행 일기를 작성해보세요!</Text>
         </View>
       )}
-
-      <BottomSheet
-        index={-1}
-        snapPoints={SNAP_POINTS}
-        ref={bottomSheetRef}
-        enablePanDownToClose={true}
-        backdropComponent={(props) => (
-          <BottomSheetBackdrop
-            {...props}
-            disappearsOnIndex={-1}
-            appearsOnIndex={0}
-          />
-        )}
-        onChange={handleSheetChanges}
-      >
-        <BottomSheetView className="px-6">
-          <Pressable
-            onPress={handleDeleteDiary}
-            className="flex-row items-center w-full px-4 py-3 bg-gray-100 rounded gap-x-2"
-          >
-            <Feather name="trash-2" size={20} color="#ef4444" />
-            <Text className="w-full text-lg font-semibold text-red-500">
-              삭제
-            </Text>
-          </Pressable>
-        </BottomSheetView>
-      </BottomSheet>
     </>
   );
 }
