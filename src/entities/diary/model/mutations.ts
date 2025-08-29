27b73a965b0a16f2f2958df46b1a372ad6454c4e
Kyung-A <nek1717@naver.com>
2025-08-29 17:58:48 +0900

    다이어리 삭제 낙관적 업데이트 추가

--- src/entities/diary/model/mutations.ts
+++ src/entities/diary/model/mutations.ts
@@ -39,11 +39,37 @@ export const useMutationDeleteDiary = () => {
 
   return useMutation({
     ...diaryMutatins.remove(),
+    onMutate: async ({ id, user_id }) => {
+      await qc.cancelQueries({ queryKey: diaryKeys.list(user_id) });
+
+      const prevData = qc.getQueriesData({
+        queryKey: diaryKeys.list(user_id),
+      })[0];
+
+      prevData.forEach(([key, list]) => {
+        if (!Array.isArray(list)) return;
+        qc.setQueryData(
+          key,
+          list.filter(d => d.id !== id),
+        );
+      });
+
+      qc.removeQueries({ queryKey: diaryKeys.detail(id), exact: true });
+
+      return { prevData };
+    },
+    onError: (_error, _data, ctx) => {
+      ctx?.prevData?.forEach(([key, data]) => qc.setQueriesData(key, data));
+    },
     onSuccess: (_, data) => {
-      qc.invalidateQueries({ queryKey: diaryKeys.list(data.user_id) });
+      qc.invalidateQueries({
+        queryKey: diaryKeys.list(data.user_id),
+        refetchType: 'active',
+      });
       qc.invalidateQueries({
         queryKey: diaryRegionKeys.byUser(data.user_id),
         exact: true,
+        refetchType: 'active',
       });
     },
   });
