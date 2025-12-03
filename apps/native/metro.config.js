7242401b398ce3bd292d6f3d047f4fefd9b12406
Kyung-A <nek1717@naver.com>
2025-12-03 16:21:40 +0900

    RN 환경설정 수정

--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -1,6 +1,20 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+const path = require('path');
+const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./src/global.css" });
+// 모노레포용 설정 추가
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '../..');
+
+config.watchFolders = [workspaceRoot];
+
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
+
+config.resolver.unstable_enableSymlinks = true;
+
+module.exports = withNativeWind(config, { input: './src/global.css' });
