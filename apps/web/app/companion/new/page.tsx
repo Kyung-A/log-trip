69527798cfd5e8e46c2040c2f5946ea18bcc3417
Kyung-A <nek1717@naver.com>
2025-12-29 21:55:24 +0900

    feat: 동행 생성/수정 form 컴포넌트만 남겨둠

--- apps/web/app/companion/new/page.tsx
+++ apps/web/app/companion/new/page.tsx
@@ -1,18 +1,5 @@
-"use client";
-
 import { CompanionForm } from "@/features/companion";
-import { ToastContainer } from "react-toastify";
 
 export default function CreateCompanion() {
-  return (
-    <>
-      <CompanionForm />
-      <ToastContainer
-        position="top-center"
-        autoClose={3000}
-        hideProgressBar
-        theme="colored"
-      />
-    </>
-  );
+  return <CompanionForm />;
 }
