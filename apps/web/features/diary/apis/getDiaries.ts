c79b61747aa944faee96220a1c4d8c7a69fdb9a2
Kyung-A <nek1717@naver.com>
2025-12-31 14:47:04 +0900

    feat: 일기 정렬 적용

--- apps/web/features/diary/apis/getDiaries.ts
+++ apps/web/features/diary/apis/getDiaries.ts
@@ -1,14 +1,17 @@
 import { supabase } from "@/shared";
 
 export const getDiaries = async () => {
-  const { data, error } = await supabase.from("diaries").select(
-    `
+  const { data, error } = await supabase
+    .from("diaries")
+    .select(
+      `
       *,
       user_info:user_id ( email, name, nickname, profile_image ),
       diary_images (id, url),
       diary_regions ( * )
     `
-  );
+    )
+    .order("created_at", { ascending: false });
 
   if (error) throw new Error(error.message);
 
