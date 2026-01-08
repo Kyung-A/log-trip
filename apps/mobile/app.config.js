4e9272ff7e5035afa57ab6e37b8c22d4ca309633
Kyung-A <nek1717@naver.com>
2026-01-08 22:48:59 +0900

    feat: 빌드 및 배포 설정 파일 수정

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -1,6 +1,6 @@
 export default ({ config }) => ({
   expo: {
-    name: "로그트립",
+    name: "logtrip",
     slug: "mobile",
     version: "1.1.0",
     orientation: "portrait",
@@ -12,7 +12,23 @@ export default ({ config }) => ({
       supportsTablet: true,
       usesAppleSignIn: true,
       bundleIdentifier: process.env.EXPO_PUBLIC_SERVICE_URL_SCHEME,
-      buildNumber: "13",
+      buildNumber: "14",
+      infoPlist: {
+        CFBundleDisplayName: "로그트립",
+        CFBundleName: "로그트립",
+        CFBundleURLTypes: [
+          {
+            CFBundleURLSchemes: [
+              `kakao${process.env.EXPO_PUBLIC_KAKAO_API_KEY}`,
+            ],
+          },
+        ],
+        LSApplicationQueriesSchemes: [
+          "kakaokompassauth",
+          "storykompassauth",
+          "kakaolink",
+        ],
+      },
     },
     android: {
       package: process.env.EXPO_PUBLIC_SERVICE_URL_SCHEME,
@@ -45,6 +61,13 @@ export default ({ config }) => ({
         "@react-native-seoul/naver-login",
         {
           urlScheme: process.env.EXPO_PUBLIC_SERVICE_URL_SCHEME,
+        },
+      ],
+      [
+        "@react-native-seoul/kakao-login",
+        {
+          kakaoAppKey: process.env.EXPO_PUBLIC_KAKAO_API_KEY,
+          kotlinVersion: "1.9.0",
         },
       ],
     ],
