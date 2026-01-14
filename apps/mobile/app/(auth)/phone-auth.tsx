25577cd0a8a6b4115eb3fb3ecf1cc11d8f85820c
Kyung-A <nek1717@naver.com>
2026-01-15 01:10:32 +0900

    feat: 회원가입 추가정보 입력 페이지 주석 삭제 및 조건문 추가

--- apps/mobile/app/(auth)/phone-auth.tsx
+++ apps/mobile/app/(auth)/phone-auth.tsx
@@ -6,15 +6,6 @@ import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import { router, useLocalSearchParams } from "expo-router";
 
-const OPTIONS = [
-  "SKT",
-  "KT",
-  "LG U+",
-  "SKT 알뜰폰",
-  "KT 알뜰폰",
-  "LG U+ 알뜰폰",
-];
-
 const DEFAULT_VALUES = {
   nickname: "",
   year_of_birth: "",
@@ -23,13 +14,7 @@ const DEFAULT_VALUES = {
   phone: "",
 };
 
-// TODO: 주석처리 한 부분은 추후 추가예정
 export default function PhoneAuthScreen() {
-  // const [selected, setSelected] = useState<string | null>(null);
-  // const [visible, setVisible] = useState(false);
-  // const [timeLeft, setTimeLeft] = useState<number>(0);
-  // const [errorVerifyCode, setErrorVerifyCode] = useState<boolean>(false);
-
   const params = useLocalSearchParams();
   const { platform, accessToken, refreshToken } = params;
 
@@ -40,89 +25,49 @@ export default function PhoneAuthScreen() {
   const createUser = async () => {
     const user = await getUser();
     const formData = getValues();
+    let resultStatus = 0;
 
-    await supabase.from("users").insert({
-      id: user?.id,
-      email: user?.email,
-      nickname: formData.nickname,
-      name: user?.user_metadata?.name ?? formData.nickname,
-      year_of_birth: formData.year_of_birth,
-      gender: formData.gender,
-      platform: platform,
-      // phone: formData.phone,
-      // mobile_carrier: formData.mobileCarrier,
-    });
+    if (platform !== "apple") {
+      const { status } = await supabase.from("users").insert({
+        id: user?.id,
+        email: user?.email,
+        nickname: formData.nickname,
+        name: user?.user_metadata?.name
+          ? user?.user_metadata?.name
+          : formData.nickname,
+        year_of_birth: formData.year_of_birth,
+        gender: formData.gender,
+        platform: platform,
+      });
+      resultStatus = status;
+    } else {
+      const { status } = await supabase
+        .from("users")
+        .update({
+          email: user?.email,
+          nickname: formData.nickname,
+          name: user?.user_metadata?.name
+            ? user?.user_metadata?.name
+            : formData.nickname,
+          year_of_birth: formData.year_of_birth,
+          gender: formData.gender,
+          platform: platform,
+        })
+        .eq("id", user?.id);
+      resultStatus = status;
+    }
 
-    await registerPushToken(user?.id);
-    router.replace({
-      pathname: "/(tabs)",
-      params: {
-        accessToken: accessToken,
-        refreshToken: refreshToken,
-      },
-    });
+    if (resultStatus === 201 || resultStatus === 200 || resultStatus === 204) {
+      await registerPushToken(user?.id);
+      router.replace({
+        pathname: "/(tabs)",
+        params: {
+          accessToken: accessToken,
+          refreshToken: refreshToken,
+        },
+      });
+    }
   };
-
-  // const handleSendSMS = handleSubmit(
-  //   async formData => {
-  //     if (formData.phone.length !== 11) {
-  //       Toast.show({
-  //         type: 'error',
-  //         text1: '올바른 휴대폰 번호가 아닙니다.',
-  //       });
-  //       return;
-  //     }
-  //     if (!formData.mobileCarrier) {
-  //       Toast.show({
-  //         type: 'error',
-  //         text1: '이동통신사 선액은 필수입니다.',
-  //       });
-  //       return;
-  //     }
-
-  //     const result = await sendSMS(formData.phone);
-  //     if (result.statusCode === '2000') {
-  //       setTimeLeft(180);
-  //     }
-  //   },
-  //   error => {
-  //     Toast.show({
-  //       type: 'error',
-  //       text1: Object.values(error)[0].message as string,
-  //     });
-  //   },
-  // );
-
-  // const handleVerifyCode = useCallback(async (code: string) => {
-  //   const phone = getValues('phone');
-
-  //   const reislt = await verifyCode({ phone: phone, code: code });
-
-  //   if (reislt.status === 200) {
-  //     return await createUser();
-  //   } else {
-  //     setErrorVerifyCode(true);
-  //   }
-  // }, []);
-
-  // const onSelect = useCallback((option: string) => {
-  //   setSelected(option);
-  //   setValue('mobileCarrier', option);
-  //   setVisible(false);
-  // }, []);
-
-  // const minutes = Math.floor(timeLeft / 60);
-  // const seconds = timeLeft % 60;
-
-  // useEffect(() => {
-  //   if (timeLeft <= 0) return;
-
-  //   const interval = setInterval(() => {
-  //     setTimeLeft(prev => prev - 1);
-  //   }, 1000);
-
-  //   return () => clearInterval(interval);
-  // }, [timeLeft]);
 
   return (
     <>
@@ -158,7 +103,6 @@ export default function PhoneAuthScreen() {
                 placeholder="닉네임 입력"
                 onChangeText={onChange}
                 value={value}
-                // editable={timeLeft <= 0}
               />
             </View>
           )}
@@ -295,184 +239,7 @@ export default function PhoneAuthScreen() {
             완료
           </Text>
         </TouchableOpacity>
-
-        {/* <View>
-        <Text style={{ fontSize: 18, fontWeight: "600" }}>휴대폰 번호</Text>
-        <Pressable
-          onPress={() => setVisible(true)}
-          style={{
-            width: "100%",
-            paddingHorizontal: 12,
-            paddingVertical: 16,
-            fontSize: 18,
-            lineHeight: 24,
-            borderWidth: 1,
-            borderColor: "#d1d5db",
-            borderRadius: 6,
-            marginTop: 6,
-          }}
-          disabled={timeLeft > 0}
-        >
-          <Text style={{ color: selected ? "#000" : "#999" }}>
-            {selected || "통신사 선택"}
-          </Text>
-        </Pressable>
-
-        <View
-          style={{
-            marginTop: 6,
-            flexDirection: "row",
-            alignItems: "stretch",
-            justifyContent: "space-between",
-            columnGap: 8,
-          }}
-        >
-          <Controller
-            control={control}
-            name="phone"
-            rules={{
-              required: "휴대폰 번호는 필수입니다.",
-            }}
-            render={({ field: { onChange, value } }) => (
-              <TextInput
-                style={{
-                  flex: 2,
-                  paddingHorizontal: 12,
-                  paddingVertical: 16,
-                  fontSize: 18,
-                  lineHeight: 24,
-                  borderWidth: 1,
-                  borderColor: "#d1d5db",
-                  borderRadius: 6,
-                }}
-                placeholder="'-'빼고 휴대폰 번호 입력"
-                onChangeText={onChange}
-                value={value}
-                editable={timeLeft <= 0}
-              />
-            )}
-          />
-
-          <Pressable
-            onPress={handleSendSMS}
-            style={{
-              flex: 1,
-              justifyContent: "center",
-              borderRadius: 6,
-              backgroundColor:
-                timeLeft > 0 || Object.values(watch()).some(v => v === "")
-                  ? "#d1d5db"
-                  : "#bfdbfe",
-            }}
-            disabled={
-              timeLeft > 0 || Object.values(watch()).some(v => v === "")
-            }
-          >
-            <Text
-              style={{
-                fontWeight: "700",
-                fontSize: 18,
-                textAlign: "center",
-                color:
-                  timeLeft > 0 || Object.values(watch()).some(v => v === "")
-                    ? "#9ca3af"
-                    : "#3b82f6",
-              }}
-            >
-              인증하기
-            </Text>
-          </Pressable>
-        </View>
-      </View> */}
-
-        {/* {timeLeft > 0 && (
-        <View>
-          <Text
-            style={{
-              marginBottom: 4,
-              fontWeight: "600",
-              color: "#ef4444",
-            }}
-          >
-            {String(minutes).padStart(2, "0")}:
-            {String(seconds).padStart(2, "0")}
-          </Text>
-          <TextInput
-            style={{
-              paddingHorizontal: 12,
-              paddingVertical: 16,
-              fontSize: 18,
-              lineHeight: 24,
-              borderWidth: 1,
-              borderColor: "#d1d5db",
-              borderRadius: 6,
-            }}
-            placeholder="인증번호 4자리 입력"
-            onChangeText={value => {
-              setErrorVerifyCode(false);
-              if (value.length === 6) {
-                handleVerifyCode(value);
-              }
-            }}
-            maxLength={6}
-          />
-          {errorVerifyCode && (
-            <Text
-              style={{
-                marginBottom: 4,
-                fontWeight: "600",
-                color: "#ef4444",
-              }}
-            >
-              인증번호가 일치하지 않습니다
-            </Text>
-          )}
-        </View>
-      )} */}
       </View>
-
-      {/* <Modal
-      visible={visible}
-      transparent
-      animationType="fade"
-      onRequestClose={() => setVisible(false)}
-    >
-      <Pressable
-        style={{
-          flex: 1,
-          backgroundColor: "rgba(0,0,0,0.46)",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-        onPress={() => setVisible(false)}
-      >
-        <View
-          style={{
-            backgroundColor: "#fff",
-            borderRadius: 6,
-            width: 320,
-          }}
-        >
-          <FlatList
-            data={OPTIONS}
-            keyExtractor={item => item}
-            renderItem={({ item, index }) => (
-              <Pressable
-                onPress={() => onSelect(item)}
-                style={{
-                  paddingHorizontal: 16,
-                  paddingVertical: 12,
-                  borderBottomWidth: index === 5 ? 0 : 1,
-                  borderColor: "#d1d5db",
-                }}
-              >
-                <Text style={{ fontSize: 18 }}>{item}</Text>
-              </Pressable>
-            )}
-          />
-        </View>
-      </Pressable>
-    </Modal> */}
     </>
   );
 }
