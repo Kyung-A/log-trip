8f84d6a4f3bd5033d2f85d6a8a8d418750f3b360
Kyung-A <nek1717@naver.com>
2026-01-17 17:31:08 +0900

    feat: 추가정보 페이지에 자기소개 입력 추가

--- apps/mobile/app/(auth)/user-info.tsx
+++ apps/mobile/app/(auth)/user-info.tsx
@@ -5,6 +5,7 @@ import { router, useLocalSearchParams } from "expo-router";
 
 const DEFAULT_VALUES = {
   nickname: "",
+  about: "",
 };
 
 export default function UserInfoScreen() {
@@ -25,9 +26,8 @@ export default function UserInfoScreen() {
         id: user?.id,
         email: user?.email,
         nickname: formData.nickname,
-        name: user?.user_metadata?.name
-          ? user?.user_metadata?.name
-          : formData.nickname,
+        name: user?.user_metadata?.name,
+        about: formData.about,
         platform: platform,
       });
       resultStatus = status;
@@ -37,9 +37,8 @@ export default function UserInfoScreen() {
         .update({
           email: user?.email,
           nickname: formData.nickname,
-          name: user?.user_metadata?.name
-            ? user?.user_metadata?.name
-            : formData.nickname,
+          about: formData.about,
+          name: user?.user_metadata?.name,
           platform: platform,
         })
         .eq("id", user?.id);
@@ -83,7 +82,6 @@ export default function UserInfoScreen() {
                   paddingHorizontal: 12,
                   paddingVertical: 16,
                   fontSize: 18,
-                  lineHeight: 24,
                   marginTop: 6,
                   borderWidth: 1,
                   borderColor: "#d1d5db",
@@ -96,15 +94,44 @@ export default function UserInfoScreen() {
             </View>
           )}
         />
+        <Controller
+          control={control}
+          name="about"
+          rules={{
+            required: "자기소개는 필수입니다.",
+          }}
+          render={({ field: { onChange, value } }) => (
+            <View>
+              <Text style={{ fontSize: 18, fontWeight: "600" }}>자기소개</Text>
+              <TextInput
+                style={{
+                  paddingHorizontal: 12,
+                  paddingVertical: 16,
+                  fontSize: 18,
+                  marginTop: 6,
+                  borderWidth: 1,
+                  borderColor: "#d1d5db",
+                  borderRadius: 6,
+                }}
+                placeholder="간단한 자기소개 작성"
+                onChangeText={onChange}
+                value={value}
+              />
+            </View>
+          )}
+        />
 
         <TouchableOpacity
           onPress={createUser}
           style={{
             justifyContent: "center",
             borderRadius: 6,
-            backgroundColor: watch("nickname") === "" ? "#e5e7eb" : "#bfdbfe",
+            backgroundColor:
+              watch("nickname") === "" || watch("about") === ""
+                ? "#e5e7eb"
+                : "#bfdbfe",
           }}
-          disabled={watch("nickname") === ""}
+          disabled={watch("nickname") === "" || watch("about") === ""}
         >
           <Text
             style={{
@@ -112,7 +139,10 @@ export default function UserInfoScreen() {
               fontSize: 18,
               fontWeight: "600",
               textAlign: "center",
-              color: watch("nickname") === "" ? "#9ca3af" : "#3b82f6",
+              color:
+                watch("nickname") === "" || watch("about") === ""
+                  ? "#9ca3af"
+                  : "#3b82f6",
             }}
           >
             완료
