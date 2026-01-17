85e4bdd2ec2a7caeafa29696ea55e902794639e5
Kyung-A <nek1717@naver.com>
2026-01-17 16:59:51 +0900

    feat: 추가정보 입력 페이지에 성별, 생일 삭제

--- apps/mobile/app/(auth)/user-info.tsx
+++ apps/mobile/app/(auth)/user-info.tsx
@@ -1,17 +1,10 @@
 import { getUser, registerPushToken, supabase } from "@/shared";
-import Ionicons from "@expo/vector-icons/Ionicons";
-import dayjs from "dayjs";
 import { Controller, useForm } from "react-hook-form";
 import { Text, TextInput, TouchableOpacity, View } from "react-native";
-import { Picker } from "@react-native-picker/picker";
 import { router, useLocalSearchParams } from "expo-router";
 
 const DEFAULT_VALUES = {
   nickname: "",
-  year_of_birth: "",
-  gender: "",
-  mobileCarrier: "",
-  phone: "",
 };
 
 export default function UserInfoScreen() {
@@ -35,8 +28,6 @@ export default function UserInfoScreen() {
         name: user?.user_metadata?.name
           ? user?.user_metadata?.name
           : formData.nickname,
-        year_of_birth: formData.year_of_birth,
-        gender: formData.gender,
         platform: platform,
       });
       resultStatus = status;
@@ -49,8 +40,6 @@ export default function UserInfoScreen() {
           name: user?.user_metadata?.name
             ? user?.user_metadata?.name
             : formData.nickname,
-          year_of_birth: formData.year_of_birth,
-          gender: formData.gender,
           platform: platform,
         })
         .eq("id", user?.id);
@@ -108,119 +97,14 @@ export default function UserInfoScreen() {
           )}
         />
 
-        <View>
-          <Text style={{ fontSize: 18, fontWeight: "600" }}>성별</Text>
-          <Controller
-            control={control}
-            name="gender"
-            rules={{
-              required: "성별은 필수입니다.",
-            }}
-            render={({ field: { onChange, value } }) => (
-              <View
-                style={{
-                  flexDirection: "row",
-                  alignItems: "center",
-                  columnGap: 24,
-                  marginTop: 8,
-                }}
-              >
-                <TouchableOpacity
-                  onPress={() => {
-                    if (!value || value === "M") {
-                      onChange("F");
-                    }
-                  }}
-                  style={{
-                    flexDirection: "row",
-                    alignItems: "center",
-                    columnGap: 6,
-                  }}
-                >
-                  {value === "F" ? (
-                    <Ionicons name="checkbox" size={26} color="#000" />
-                  ) : (
-                    <Ionicons name="checkbox-outline" size={26} color="#ccc" />
-                  )}
-                  <Text style={{ fontSize: 18 }}>여성</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity
-                  onPress={() => {
-                    if (!value || value === "F") {
-                      onChange("M");
-                    }
-                  }}
-                  style={{
-                    flexDirection: "row",
-                    alignItems: "center",
-                    columnGap: 6,
-                  }}
-                >
-                  {value === "M" ? (
-                    <Ionicons name="checkbox" size={26} color="#000" />
-                  ) : (
-                    <Ionicons name="checkbox-outline" size={26} color="#ccc" />
-                  )}
-                  <Text style={{ fontSize: 18 }}>남성</Text>
-                </TouchableOpacity>
-              </View>
-            )}
-          />
-        </View>
-
-        <View>
-          <Text style={{ fontSize: 18, fontWeight: "600" }}>태어난 연도</Text>
-          <Controller
-            control={control}
-            name="year_of_birth"
-            rules={{
-              required: "태어난 연도는 필수입니다.",
-            }}
-            render={({ field: { onChange, value } }) => {
-              const curYear = dayjs().get("year");
-              const minYear = 1900;
-              const maxYear = curYear - 18;
-
-              const years = [];
-              for (let y = maxYear; y >= minYear; y--) {
-                years.push(y);
-              }
-
-              return (
-                <View style={{ width: "100%" }}>
-                  <Picker
-                    selectedValue={value}
-                    onValueChange={onChange}
-                    style={{ height: 180 }}
-                  >
-                    {years?.map((year) => (
-                      <Picker.Item key={year} label={`${year}`} value={year} />
-                    ))}
-                  </Picker>
-                </View>
-              );
-            }}
-          />
-        </View>
-
         <TouchableOpacity
           onPress={createUser}
           style={{
             justifyContent: "center",
             borderRadius: 6,
-            backgroundColor:
-              watch("nickname") === "" ||
-              watch("year_of_birth") === "" ||
-              watch("gender") === ""
-                ? "#e5e7eb"
-                : "#bfdbfe",
+            backgroundColor: watch("nickname") === "" ? "#e5e7eb" : "#bfdbfe",
           }}
-          disabled={
-            watch("nickname") === "" ||
-            watch("year_of_birth") === "" ||
-            watch("gender") === ""
-          }
+          disabled={watch("nickname") === ""}
         >
           <Text
             style={{
@@ -228,12 +112,7 @@ export default function UserInfoScreen() {
               fontSize: 18,
               fontWeight: "600",
               textAlign: "center",
-              color:
-                watch("nickname") === "" ||
-                watch("year_of_birth") === "" ||
-                watch("gender") === ""
-                  ? "#9ca3af"
-                  : "#3b82f6",
+              color: watch("nickname") === "" ? "#9ca3af" : "#3b82f6",
             }}
           >
             완료
