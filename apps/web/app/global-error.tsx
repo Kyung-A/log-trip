05377efbfdfab943399b7e88bf64f353ec55162a
Kyung-A <nek1717@naver.com>
2026-03-16 21:50:30 +0900

    feat: error 페이지 스타일 변경

--- apps/web/app/global-error.tsx
+++ apps/web/app/global-error.tsx
@@ -15,11 +15,19 @@ export default function GlobalError({
 
   return (
     <html lang="ko">
-      <body style={{ margin: 0, padding: 0 }}>
+      <body
+        style={{
+          margin: 0,
+          padding: 0,
+          width: "100vw",
+          height: "100vh",
+          overflow: "hidden",
+        }}
+      >
         <main
           style={{
-            width: "100vw",
-            height: "100vh",
+            width: "100%",
+            height: "100%",
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
