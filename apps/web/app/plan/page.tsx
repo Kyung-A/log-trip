4bdccdfda4d63416e35b4749f0b243c498c4d9a7
Kyung-A <nek1717@naver.com>
2026-04-12 22:17:20 +0900

    feat: 여행 일정 목록 페이지 퍼블리싱

--- apps/web/app/plan/page.tsx
+++ apps/web/app/plan/page.tsx
@@ -1,19 +1,80 @@
-import Image from "next/image";
+import { ChevronRightIcon } from "lucide-react";
+import Link from "next/link";
 
-export default async function Plan() {
+interface IPlanProps {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+
+export default async function Plan({ searchParams }: IPlanProps) {
+  const params = await searchParams;
+  const currentTab = params.tab || "all";
+
   return (
-    <div className="flex flex-col items-center justify-center gap-6 w-full min-h-screen h-ull bg-beige">
-      <Image
-        src="/images/logo/logo.png"
-        className="object-cover w-32 h-32"
-        alt="logo"
-        width={0}
-        height={0}
-        sizes="100vw"
-      />
-      <p className="text-base whitespace-pre-wrap text-center text-zinc-700">
-        여행 일정을 계획하는 서비스를 준비중입니다.
-      </p>
+    <div className="bg-beige min-h-screen w-full overflow-hidden">
+      <header className="p-4 sticky top-0 z-30 bg-white">
+        <h1 className="text-3xl font-semibold">여행 일정</h1>
+        <nav className="mt-2 flex items-center gap-x-2 flex-nowrap overflow-x-auto">
+          <Link
+            href="?tab=all"
+            className={`px-4 rounded-full py-0.5 text-base cursor-pointer border shrink-0 ${
+              currentTab === "all"
+                ? "bg-[#e9dcd9] border-[#e9dcd9] text-[#a38f86] font-semibold"
+                : "text-zinc-500 border-zinc-300"
+            }`}
+            scroll={false}
+          >
+            전체
+          </Link>
+          <Link
+            href="?tab=ing"
+            className={`px-4 rounded-full py-0.5 text-base cursor-pointer border shrink-0 ${
+              currentTab === "community"
+                ? "bg-[#e9dcd9] border-[#e9dcd9] text-[#a38f86] font-semibold"
+                : "text-zinc-500 border-zinc-300"
+            }`}
+            scroll={false}
+          >
+            진행중인 여행
+          </Link>
+          <Link
+            href="?tab=diary"
+            className={`px-4 rounded-full py-0.5 text-base cursor-pointer border shrink-0 ${
+              currentTab === "early"
+                ? "bg-[#e9dcd9] border-[#e9dcd9] text-[#a38f86] font-semibold"
+                : "text-zinc-500 border-zinc-300"
+            }`}
+            scroll={false}
+          >
+            예정
+          </Link>
+          <Link
+            href="?tab=community"
+            className={`px-4 rounded-full py-0.5 text-base cursor-pointer border shrink-0 ${
+              currentTab === "end"
+                ? "bg-[#e9dcd9] border-[#e9dcd9] text-[#a38f86] font-semibold"
+                : "text-zinc-500 border-zinc-300"
+            }`}
+            scroll={false}
+          >
+            지난 여행
+          </Link>
+        </nav>
+      </header>
+      <ul className="w-full p-4 flex flex-col gap-y-4">
+        {[1, 2, 3, 4, 5].map((day) => (
+          <li key={day} className="px-8 py-4 bg-white rounded-full">
+            <Link href="#" className="flex items-center justify-between">
+              <div>
+                <h2 className="text-lg font-semibold">인도네시아 발리 여행</h2>
+                <p className="text-sm text-zinc-500">
+                  2023년 5월 15일 - 2023년 5월 20일
+                </p>
+              </div>
+              <ChevronRightIcon size={26} color="#d4d4d8" />
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
