f9f91abffb1922caf4c6fddf194490a71dd2277f
Kyung-A <nek1717@naver.com>
2026-04-23 21:40:07 +0900

    feat: 구버전 호환 리다이렉트 추가

--- apps/web/app/public-diary/page.tsx
+++ apps/web/app/public-diary/page.tsx
@@ -1,20 +1,5 @@
-import Image from "next/image";
+import { redirect } from "next/navigation";
 
-// TODO: 이전 버전 사용자를 위한 임시 유지 페이지, 추후 삭제 예정
-export default async function PublicDiary() {
-  return (
-    <div className="flex flex-col items-center justify-center gap-6 w-full min-h-screen h-ull bg-beige">
-      <Image
-        src="/images/logo/logo.png"
-        className="object-cover w-32 h-32"
-        alt="logo"
-        width={0}
-        height={0}
-        sizes="100vw"
-      />
-      <p className="text-base whitespace-pre-wrap text-center text-zinc-700">
-        여행 일정을 계획하는 서비스를 준비중입니다.
-      </p>
-    </div>
-  );
+export default function PublicDiary() {
+  redirect("/plan");
 }
